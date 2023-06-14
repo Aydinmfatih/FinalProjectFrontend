9a8d9a78d49a6fcdfc37edbd8b33e548be9f5d94
aydinmfatih <aydinmfatih@gmail.com>
2023-06-14 03:40:01 +0300

    updated

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44317/api/products/getall';
+  productResponseModel:ProductResponseModel={
+    data:this.products,
+    message:"",
+    success:true
 
-  products:Product[] = [];
-  
+  } ;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products= response.data;
+      });
+  }
 }
